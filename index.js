ed8595b297fa81e79594d3c22e90b83401553854
GabbyJ <GabbyJ424@gmail.com>
2019-10-04 14:17:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
     });
     
     
-    document.querySelectorAll(".drum")[i].addEventListener("touchstart", function (){
+    document.querySelectorAll(".drum")[i].addEventListener("touchstart", function (evt){
+        
+        evt.preventDefault();
         
         var buttonInnerHTML = this.innerHTML;
 
